c0a4a8ec7938046a6efec75ba3ba0204f537bc7f
Joseph Yølk Chiocchi <joe@yolk.cc>
2012-12-03 14:24:45 -0600

    incremental

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.dev = {
 exports.prod = {
     port: 14858, // Nodester port
     client_port: 80, // Websockets talk on port 80 on Nodester, regardless of listen port
-    mongoose_auth: 'mongodb://mongodb@localhost/subway'
+    mongoose_auth: 'mongodb://nodejitsu_yolk:2ol46qip04agri14gstbgr0at@ds039267.mongolab.com:39267/nodejitsu_yolk_nodejitsudb8499178618'
 }
 
 exports.misc = {
